187c619b0af75be26a110f5ef0c59352617e6b28
muhammad45401 <saadjani3333@gmail.com>
2023-12-01 00:20:01 +0500

    string mathods

---
+++ app.js
@@ -0,0 +1,34 @@
+document.write("<h3> Q:-1 </h3>");
+//var fName = prompt("Enter youer first name ?");
+//var lName = prompt(" Enter your last name ?");
+
+var fullName= fName+lName;
+document.write(fullName);
+
+document.write("<h3> Q:-2 </h3>");
+
+//var a= prompt("enter you fav mobile");
+document.write(a.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
